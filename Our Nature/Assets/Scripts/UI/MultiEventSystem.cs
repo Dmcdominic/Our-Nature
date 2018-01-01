d33a844c407e7569d7a6c2e3b33d8c8741987527
Dmcdominic <dmcdominic@gmail.com>
2018-01-01 12:22:14 -0500

    Edits to Crafting UI, Starting on MultiEventSystem

---
+++ Our Nature/Assets/Scripts/UI/MultiEventSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MultiEventSystem : EventSystem {
+
+	protected override void OnEnable() {
+		// Do not assign EventSystem.current
+	}
+
+	protected override void Update() {
+		EventSystem originalCurrent = EventSystem.current;
+		current = this; // Temporarily assign this EventSystem to be the globally current one
+		base.Update();
+		current = originalCurrent;
+	}
+}
